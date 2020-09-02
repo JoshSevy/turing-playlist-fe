0c461c92becad9a00b72dfa2a6ad5bfcb91292d7
Joshua Sevy <joshuasevy@outlook.com>
2020-09-02 12:03:13 -0600

    Add app test to check if new songs loaded on submit

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 import { getPlaylist } from '../helpers/api'
 
+jest.mock('../helpers/api');
+
+
+
 describe('App Component', () => {
-  it('should allow user to add a song to the songsQueue', () => {
+  it('should allow user to add a song to the songsQueue', async () => {
+
+    getPlaylist.mockResolvedValueOnce(
+      [{
+      artistName: 'ABBA',
+      songName: 'Dancing Queen',
+      url: 'http:fakeurl.com'
+    }]
+    )
+
     render(      
       <App />
       )
 
-    screen.debug()
-      
     const songInput = screen.getByPlaceholderText(/input song name/i);
     const artistInput = screen.getByPlaceholderText(/input artist name/i);
     const linkInput = screen.getByPlaceholderText(/input url/i);
@@ -22,9 +34,14 @@ describe('App Component', () => {
     fireEvent.change(linkInput, { target: { value: 'http' } });
 
     fireEvent.click(button)
-      
-
     
+
+    const song1 = await waitFor( () => screen.getByText(/dancing queen/i));
+    const song2 = await waitFor( () => screen.getByText(/swear/i));
+
+    expect(song1).toBeInTheDocument();
+    expect(song2).toBeInTheDocument();
+
   })
 })
 
